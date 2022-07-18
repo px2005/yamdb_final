de039bbd2a177064ffd801c84116f301f1330413
Александр Горыничев <px2005@yandex.ru>
2022-07-18 16:27:38 +0300

    v3.05

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,9 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from api_yamdb import settings
 from core.models import BaseTextModel
 from core.validators import validate_date
+from api_yamdb import settings
 
 
 class Category(models.Model):
