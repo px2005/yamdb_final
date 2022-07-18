9ab8c9441d99b70c993f284eba4cefdf2c8d8f49
Александр Горыничев <px2005@yandex.ru>
2022-07-18 18:15:08 +0300

    v3.15

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,8 +6,7 @@ class IsModerator(BasePermission):
     def has_permission(self, request, view):
         return (
                 request.user.is_authenticated
-                and request.user.role == Roles.moderator.name
-        )
+                and request.user.role == Roles.moderator.name)
 
 
 class IsAdmin(BasePermission):
@@ -15,8 +14,7 @@ class IsAdmin(BasePermission):
         return (
                 request.user.is_superuser
                 or request.user.is_authenticated
-                and request.user.role == Roles.admin.name
-        )
+                and request.user.role == Roles.admin.name)
 
 
 class ReadOnly(BasePermission):
@@ -48,8 +46,7 @@ class ReadOnlyOrAuthorModeratorAdmin(BasePermission):
                 or obj.author == request.user
                 or not request.user.is_anonymous
                 and request.user.role in [Roles.moderator.name,
-                                          Roles.admin.name]
-        )
+                                          Roles.admin.name])
 
 
 class IsMeAndSuperUserAndAdmin(BasePermission):
@@ -64,7 +61,5 @@ class IsMeAndSuperUserAndAdmin(BasePermission):
                 and any((
             username_me,
             request.user.role == Roles.admin.name,
-            request.user.is_superuser
-        ))
-        )
+            request.user.is_superuser)))
         return result
