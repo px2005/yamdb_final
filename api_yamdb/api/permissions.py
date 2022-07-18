cebc88fd5a8006d1407a308c44634fc2df596044
Александр Горыничев <px2005@yandex.ru>
2022-07-18 19:01:06 +0300

    v3.16

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,20 +1,20 @@
-from users.enums import Roles
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from users.enums import Roles
 
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.user.is_authenticated
-                and request.user.role == Roles.moderator.name)
+        return (request.user.is_authenticated
+                and request.user.role == Roles.moderator.name
+                )
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.user.is_superuser
+        return (request.user.is_superuser
                 or request.user.is_authenticated
-                and request.user.role == Roles.admin.name)
+                and request.user.role == Roles.admin.name
+                )
 
 
 class ReadOnly(BasePermission):
@@ -40,13 +40,13 @@ class ReadOnlyOrAuthorModeratorAdmin(BasePermission):
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in SAFE_METHODS
+        return (request.method in SAFE_METHODS
                 or request.user.is_superuser
                 or obj.author == request.user
                 or not request.user.is_anonymous
                 and request.user.role in [Roles.moderator.name,
-                                          Roles.admin.name])
+                                          Roles.admin.name]
+                )
 
 
 class IsMeAndSuperUserAndAdmin(BasePermission):
@@ -56,10 +56,10 @@ class IsMeAndSuperUserAndAdmin(BasePermission):
         except AttributeError:
             username_me = False
 
-        result = (
-                request.user.is_authenticated
-                and any((
-            username_me,
-            request.user.role == Roles.admin.name,
-            request.user.is_superuser)))
+        result = (request.user.is_authenticated
+                  and any((username_me,
+                           request.user.role == Roles.admin.name,
+                           request.user.is_superuser
+                           ))
+                  )
         return result
